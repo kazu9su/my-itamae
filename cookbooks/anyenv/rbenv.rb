259b4547876eb37348800dc5f79adae123486ef1
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-16 09:59:40 +0900

    up

--- cookbooks/anyenv/rbenv.rb
+++ cookbooks/anyenv/rbenv.rb
@@ -9,4 +9,17 @@ end
 
 execute "Install Ruby #{node[:rbenv][:version]}" do
   user node[:anyenv][:user]
+  command <<-CMD
+    . #{node[:anyenv][:shell_profile]};
+    rbenv install #{node[:rbenv][:version]}
+  CMD
+  not_if "test -e #{node[:anyenv][:install_dir]}/envs/rbenv/versions/#{node[:rbenv][:version]}"
+end
+
+execute "Set Perl #{node[:rbenv][:version]} as default" do
+  user node[:anyenv][:user]
+  command <<-CMD
+    . #{node[:anyenv][:shell_profile]};
+    rbenv global #{node[:rbenv][:version]}
+  CMD
 end
